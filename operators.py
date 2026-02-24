a9c57bfb34291650c27800a900bd03bde50e4426
aishwaryash775 <aishwaryash880@gmail.com>
2026-02-24 16:27:58 +0530

    operators types

--- operators.py
+++ operators.py
@@ -17,10 +17,40 @@ print(a // b)  # Floor Division
 7
 49
 0
-'''
+
 #average of 3 numbers
 a = 7
 b = 20
 c = 21
 average = (a + b + c) / 3
 print(average)  # Output: 16.0
+
+#relational operators
+a = 7
+b = 20
+print(a > b)  # Greater than
+print(a < b)  # Less than
+print(a >= b)  # Greater than or equal to
+print(a <= b)  # Less than or equal to
+print(a == b)  # Equal to
+print(a != b)  # Not equal to
+#output
+False
+True
+False
+True
+False
+True
+'''
+
+#logical operators 
+a = True
+b = False   
+
+print('a and b:', a and b)  # Logical AND
+print('a or b:', a or b)   # Logical OR
+print('not a:', not a )  # Logical NOT
+#output
+False
+True
+False
