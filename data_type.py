b4b87a39b18692787a3815be8594d96dc260600d
aishwaryash775 <aishwaryash880@gmail.com>
2026-02-24 13:14:17 +0530

    first commit

---
+++ data_type.py
@@ -0,0 +1,12 @@
+name = 'aishwarya'
+Age = 23
+year_of_birth = 2003
+is_new_user = True
+Favorite_fruit = ['blueberry', 'strawberry', 'mango']
+
+print(name)
+print(Age)
+
+print(year_of_birth)
+print(is_new_user)
+print(Favorite_fruit)
