84c9f0fc03e6a5827f77b1ea1a4ed4838200076e
aishwaryash775 <aishwaryash880@gmail.com>
2026-02-24 16:14:26 +0530

    opeartors

---
+++ operators.py
@@ -0,0 +1,26 @@
+'''
+a = 7
+b = 20
+c = a + b
+
+print(c)  # Addition
+print(a - b)  # Subtraction
+print(a * b)  # Multiplication
+print(a / b)  # Division
+print(a % b)  # Modulus
+print(a ** 2)  # Exponentiation
+print(a // b)  # Floor Division
+#output 27
+-13
+140
+0.35
+7
+49
+0
+'''
+#average of 3 numbers
+a = 7
+b = 20
+c = 21
+average = (a + b + c) / 3
+print(average)  # Output: 16.0
