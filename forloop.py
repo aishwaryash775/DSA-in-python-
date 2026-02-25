1cf6882ad79ef5b37f7282b3ab07edf61f7a0ed9
aishwaryash775 <aishwaryash880@gmail.com>
2026-02-25 13:20:37 +0530

    for loop

---
+++ forloop.py
@@ -0,0 +1,14 @@
+a = [6,7,6,3,5,3,2,1,10]
+
+for element in a:
+    print(element*2)
+
+#more for loop examples
+for i in range(10):
+    print(i)
+for i in range(1, 11):
+    print(i)
+for i in range(1, 11, 2):
+    print(i)
+for i in range(10, 0, -1):
+    print(i)
