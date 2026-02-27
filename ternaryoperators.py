35016fed41335e495cfbe0f0ef3edd7dfbd02cfd
aishwaryash775 <aishwaryash880@gmail.com>
2026-02-27 14:27:51 +0530

    TERNARY OPERATIONS

---
+++ ternaryoperators.py
@@ -0,0 +1,13 @@
+#ternary operators 
+age = int(input("ENTER YOUR AGE"))
+result ="Eligible" if age>=18 else "Not eligible"
+print(result)
+'''
+print("Enter your age:")
+age = int(input())
+
+# The Ternary Operator
+status = "Adult" if age >= 18 else "Minor"
+
+print(status)
+'''
---
+++ tuples.py
@@ -0,0 +1,6 @@
+ #tuples example
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple)  # Output: (1, 2, 3, 4, 5)
+print(my_tuple[0])  # Output: 1
+print(my_tuple[1:4])  # Output: (2, 3, 4)
+print(len(my_tuple))  # Output: 5
